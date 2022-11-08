05c27879b87e68bdf199d10af86824a0d2c9199c
Sivloc <clovis.bergeret@laposte.net>
2022-11-08 14:56:15 +0100

    ok c'est bo

--- game.js
+++ game.js
@@ -1,1 +1,5 @@
 let map = L.map('map').setView([30.050, 31.235], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
