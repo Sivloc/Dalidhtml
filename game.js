dd9be869a122d8a31fb986bce8733155597333ad
Sivloc <clovis.bergeret@laposte.net>
2022-11-08 16:09:38 +0100

    debut js inventaire

--- game.js
+++ game.js
@@ -3,3 +3,44 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+let slot1 = document.getElementById("slot1");
+let slot2 = document.getElementById("slot2");
+let slot3 = document.getElementById("slot3");
+let slot4 = document.getElementById("slot4");
+let slot5 = document.getElementById("slot5");
+
+class item {
+    constructor(img,usable){
+        this.img = "..\images\bloc_default";
+        this.usable = false;
+    }
+}
+
+function slot_fill(n,item){
+    str = item.img;
+    n.innerHTML = "<img src="|str|"></img>";
+}
+
+function slot_remove(n){
+    n.innerHTML = null;
+}
+
+function addItem(item){
+    item.usable = true;
+    if(slot1.innerHTML == null){
+        slot_fill(slot1,item);
+    }
+    else if(slot2.innerHTML == null){
+        slot_fill(slot2,item);
+    }
+    else if(slot3.innerHTML == null){
+        slot_fill(slot3,item);
+    }
+    else if(slot4.innerHTML == null){
+        slot_fill(slot4,item);
+    }
+    else if(slot5.innerHTML == null){
+        slot_fill(slot5,item);
+    }
+}
