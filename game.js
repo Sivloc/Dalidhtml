7a8b7b54c3ede94bf1f8f29171c1a3508867eaaf
Sivloc <clovis.bergeret@laposte.net>
2022-11-14 15:21:16 +0100

    customs markers et bordure de la carte

--- game.js
+++ game.js
@@ -27,6 +27,22 @@ function slot_fill(n,item){
 function slot_remove(n){
     n.innerHTML = null;
 }
+/*MARKERS DU JEU*/
+let LeafIcon = L.Icon.extend({
+    options: {
+        shadowUrl: '../images/leaf-shadow.png',
+        iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76]
+    }
+});
+let greenIcon = new LeafIcon({iconUrl: '../images/leaf-green.png'});
+let icone1 = new LeafIcon({iconUrl: '../images/img1'});
+let icone2 = new LeafIcon({iconUrl: '../images/img2'});
+
+L.marker([30.050, 31.235], {icon: greenIcon}).addTo(map).bindPopup("Résouds mon énigme pour une pipe");
 
 function addItem(item){
     item.usable = true;
