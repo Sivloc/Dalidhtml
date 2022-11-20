0bf9133ae8d85e4555773aba1e7fa7738802d07a
clement.sicot24 <clement.sicot24@gmail.com>
2022-11-20 20:12:48 +0100

    P'tite modifs

---
+++ login.php
@@ -0,0 +1,4 @@
+<?php 
+    connexion = new PDO('pgsql:host=localhost;port=5432;dbname=login', 'postgres', 'postgres');
+    
+?>
--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 let form_login = document.getElementById('login');
 let form_button = document.getElementById('start');
 
-form_login.addEventListener('submit', commencer);
-form_button.addEventListener('click', commencer);
 form_login.addEventListener('submit', bloque);
 form_button.addEventListener('click', bloque);
+form_login.addEventListener('submit', commencer);
+form_button.addEventListener('click', commencer);
+
+let champ_pseudo = form_login.elements["pseudo"];
+
+function bloque(event){
+
+    let form_OK = true;
+
+    if(champ_pseudo.value == ''){
+        form_OK = false;
+    }
+
+    if(!form_OK){
+        event.preventDefault();
+        alert("Mets ton pseudo stp");
+    }
+    console.log("C'est NON");
+}
 
 function commencer(event){
     event.preventDefault();
@@ -12,18 +29,3 @@ function commencer(event){
     window.location.href="game.html";
 }
 
-let champ_pseudo = form_login.elements["pseudo"];
-
-function bloque(event){
-    
-    let form_OK = true;
-
-    if(champ_pseudo.value.length == 0){
-        form_OK = false;
-    }
-
-    if(!form_OK){
-        event.preventDefault();
-    }
-    console.log("C'est NON");
-}
