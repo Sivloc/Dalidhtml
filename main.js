0e81f7df5bfe28c7216616b15b1246899099849d
Sivloc <clovis.bergeret@laposte.net>
2022-11-28 19:00:18 +0100

    rver

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let form_login = document.getElementById('login');
 let form_button = document.getElementById('start');
 
-form_login.addEventListener('submit', test);
-form_button.addEventListener('click', test);
+form_login.addEventListener('submit', entrée)
+form_button.addEventListener('click', cliquage);
 /*form_login.addEventListener('submit', commencer);
 form_button.addEventListener('click', commencer);
 
@@ -31,7 +31,28 @@ function commencer(event){
     window.location.href="game.html";
 }*/
 
-function test(event){
+function cliquage(event){
+    let form_OK = true;
+    champ_pseudo = form_login.value;
+    console.log(champ_pseudo);
+    if(champ_pseudo == ''){
+        form_OK = false;
+        console.log("C'est NON");
+    }
+    else if(champ_pseudo != ''){
+        form_OK = true;
+        event.preventDefault();
+        console.log("Let's goo");
+        window.location.href="game.html";
+    }
+    if(!form_OK){
+        event.preventDefault();
+        alert("Mets ton pseudo stp");
+    }
+}
+
+function entrée(event){
+    event.preventDefault();
     let form_OK = true;
     champ_pseudo = form_login.value;
     console.log(champ_pseudo);
