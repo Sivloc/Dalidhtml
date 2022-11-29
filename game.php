54779a05ebb1d476cb58d8a2bfbee47ef42bfe32
Sivloc <clovis.bergeret@laposte.net>
2022-11-29 20:59:15 +0100

    game.html --> game.php

--- game.php
+++ game.php
@@ -1,5 +1,47 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=alex-brush:400" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=acme:400" rel="stylesheet" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css"
+    integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14="
+    crossorigin=""/>
+    <link rel="stylesheet" href="game.css"/>
+    <title>Escape Dalida</title>
+</head>
+<body>
+    <ul id="barre">
+        <li class="test" id="accueil">
+            <a href="main.html" class="button_link"><b>Jouer</b></a>
+        </li>
+        <li class="test" id="hof">
+            <a href="hof.html" class="button_link"><b>Hall of Fame</b></a>
+        </li>
+        <li class="test" id="chansons"><b>Chansons</b></li>
+        <li class="test" id="ressources"><b>Ressources</b></li>
+        <li id="eg"><b>Escape Game</b></li>
+    </ul>
+    
+    <div id="jeu">
+        <div id="map"></div>
 
+        <div id="inventaire">
+            <div id="titre_inv"><p>Inventaire</p></div>
+            <div id="slot1"></div>
+            <div id="slot2"></div>
+            <div id="slot3"></div>
+            <div id="slot4"></div>
+            <div id="slot5"></div>
+        </div>
+    </div>
 
-
-?>
+    <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js"
+     integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg="
+     crossorigin=""></script>
+     <script src="game.js"></script>
+</body>
+</html>
