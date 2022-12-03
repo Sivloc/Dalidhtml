c3eb4b15acf37e25be7a88f3aed3d47e32aa6e38
clement.sicot24 <clement.sicot24@gmail.com>
2022-12-03 19:35:09 +0100

    Classement

--- hof.js
+++ hof.js
@@ -3,5 +3,5 @@ let pseudo = window.localStorage.getItem('pseudo');
 let p1 = document.getElementById("pseudo1");
 let s1 = document.getElementById("score1");
 p1.innerHTML = pseudo;
-s1.innerHTML = score;
+s1.innerHTML = score + " s";
 console.log(pseudo);
