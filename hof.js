b211518c5009d308b6071acddb2c1bbe78b889af
clement.sicot24 <clement.sicot24@gmail.com>
2022-12-04 21:14:57 +0100

    vvc ng

--- hof.js
+++ hof.js
@@ -4,5 +4,5 @@ let p1 = document.getElementById("pseudo1");
 let s1 = document.getElementById("score1");
 p1.innerHTML = pseudo;
 s1.innerHTML = score + " s";
-console.log(pseudo);
+console.log(pseudo);*/
 
